8fdd100e9015961eba5050b36fd3bab19ed9092f
natashafir <flyelin@gmail.com>
2020-11-27 23:03:27 +0200

    Add constructor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 // Enemies our player must avoid
-var Enemy = function() {
+let Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
@@ -24,18 +24,36 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+let Player = function(x, y){
+    this.x = x;
+    this.y = y;
+    this.sprite = ('images/char-boy.png');
+};
+Player.prototype.update = function(){
 
+};
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+let player = new Player(200, 400);
 
 // Now instantiate your objects.
+let enemyBoy = new Enemy();
+
+const allEnemies = [
+    enemyBoy
+];
 // Place all enemy objects in an array called allEnemies
+
 // Place the player object in a variable called player
 
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+document.addEventListener('keyup', e => {
+    let allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
